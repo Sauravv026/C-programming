0d553684641994ee3d37883a5ac68cbf7c41993e
Sauravv026 <sauravparajuli026@gmail.com>
2026-08-16 06:58:59 +0545

    Create Integers.c

---
+++ Variables/Integers.c
@@ -0,0 +1,18 @@
+/*
+In C, integers are whole numbers without any decimal points. They are one of the most common data types you'll work with.
+
+Declaring, initializing, and modifying integers:
+
+int age; // Declaring
+int score = 100; // Declaring and initializing
+score = 90; // Modifying
+Print an integer using printf:
+
+printf("%d", score);
+The int type stores whole numbers.
+  CODE: */
+#include<stdio.h>
+int main(){
+    int num=50;
+    printf("The value is: %d", num);
+}
